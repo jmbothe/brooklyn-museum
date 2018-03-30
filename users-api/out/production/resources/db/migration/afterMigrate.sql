c7e3805ca68a206da5faf5e36522db91236d2b87
jmbothe <jeffrymbothe@gmail.com>
2018-03-30 17:49:04 -0400

    add seed data to users-api

---
+++ users-api/out/production/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,37 @@
+insert into USERS (EMAIL, FULL_NAME) values ('ltomsuv@reverbnation.com', 'Lisabeth Tomsu');
+insert into USERS (EMAIL, FULL_NAME) values ('starplyw@nba.com', 'Stacey Tarply');
+insert into USERS (EMAIL, FULL_NAME) values ('rrigglesfordx@sciencedirect.com', 'Rebecca Rigglesford');
+
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 1);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 2);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 3);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 4);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 5);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 6);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 7);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 8);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 9);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (1, 10);
+
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 1);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 2);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 3);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 4);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 5);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 6);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 7);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 8);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 9);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (2, 10);
+
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 11);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 12);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 13);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 14);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 15);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 16);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 17);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 18);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 19);
+insert into FAVORITES (USER_ID, OBJECT_ID) values (3, 20);
+
